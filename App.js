25f74f0992a8465fda1e6be15367fbe383f8fac0
Arjun Solanki <arjunsolanki2148@gmail.com>
2024-10-17 20:02:03 +0530

    adding all

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // 1. We will create a heading using react //2. we will create a root using
 // 2. react dom because we have to render the heading inside the root and show it to
 // 3. browser and all dom related thing is done by React-Dom //3. render the heading
